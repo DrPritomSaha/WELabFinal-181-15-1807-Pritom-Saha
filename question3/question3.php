b7176325f45aab62dd101b556a98f8041dd6bc1d
pritom1807 <pritom15-1807@diu.edu.bd>
2021-04-12 15:51:00 +0600

    This Is for our WE Lab Final

---
+++ question3/question3.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Bootstrap Theme Company Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Teacher Info</a></li>
+                    <li><a href="#">Notice Board</a></li>
+                    <li><a href="#">Contact us</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div id="about" class="container-fluid">
+  <div class="row">
+    <div class="col-sm-8">
+      <h2>Welcome</h2><br>
+      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <br><button class="btn btn-default btn-lg">More </button>
+    </div>
+
+  </div>
+</div>
+    
+
+    <footer class="container-fluid text-center">
+        <p>Made By &#169 Pritom Saha</p>
+    </footer>
+
+
+</body>
+
+</html>
---
+++ question6/insert.php
@@ -0,0 +1,29 @@
+<?php
+include ("connection.php");
+
+
+$f_name = $_POST['f_name'];
+$l_name = $_POST['l_name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$gender = $_POST['gender'];
+
+
+$sql = "insert into signup values('','$f_name', '$l_name', '$email', '$password', '$gender' )";
+
+$data = mysqli_query($conn,$sql);
+
+if ($data) {
+    
+	
+//	header("location: /inkINlink/home.php");
+	
+ echo "hello";
+	
+	
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+
+?>
